43a80932437da35f3e868e910a2e8aac9e77fad5
Julio Arath Rosales <35709739+arath11@users.noreply.github.com>
2020-05-26 12:39:58 -0500

    can handle larger instances

--- src/Grafo.java
+++ src/Grafo.java
@@ -5,45 +5,19 @@ import java.util.Scanner;
 
 public class Grafo {
 	public static int n, cont = 1, contador = 0;
-	public static int[] v;
 	public static ArrayList<Integer> array1 = new ArrayList<Integer>(),
-									 array2 = new ArrayList<Integer>(),
-									 array3 = new ArrayList<Integer>();
-	public static ArrayList<Individuo> population;
+			array2 = new ArrayList<Integer>(),
+			array3 = new ArrayList<Integer>(),
+			v = new ArrayList<Integer>();
+	public static ArrayList<Individuo> population = new ArrayList<Individuo>(),
+			population2 = new ArrayList<Individuo>();
 	public static Integer minDefinitivo, minTmp, cantIndividuos, cantPoblaciones;
 	public static ArrayList<ArrayList<Integer>> pob2 = new ArrayList<ArrayList<Integer>>();
 
-	public static void poblacion(Integer maxArista, ArrayList<Integer> arre) {
-		population = new ArrayList<Individuo>();
-
-		Integer aux = 0;
+	public static void poblacion(ArrayList<Integer> arre) {
 		for (Integer i = 0; i < cantIndividuos; i++) {
-			aux = 0;
 			Permutacion.Permutacion(arre);
-			System.out.print("Esto vale el máx arista: "+ maxArista);
-
-			ArrayList<Integer> listaAux = new ArrayList<Integer>();
-
-			for (Integer j = 0; j < n; j++) {
-				if (j == maxArista) {
-					System.out.print("Entré:");
-					listaAux.add(1);
-				}
-				if (j != maxArista) {
-
-					listaAux.add(Permutacion.genotype.get(aux));
-					aux++;
-				}
-				if (aux == Permutacion.genotype.size()) {
-					break;
-				}
-			}
-			
-			for(int k = 0; k < listaAux.size();k++) {
-				System.out.print(listaAux.get(k) + " ");
-			}
-			
-			population.add(new Individuo(busquedaLocal(listaAux, sumMinIndividuo(listaAux))));
+			population.add(new Individuo(busquedaLocal(arre, sumMinIndividuo(arre))));
 		}
 	}
 
@@ -55,38 +29,29 @@ public class Grafo {
 		for (Integer i = 0; i < mutanteAuxiliar.size() / 2; i++) {
 			Integer datotmp = random.nextInt(mutanteAuxiliar.size());
 			Integer segundodato = random.nextInt(mutanteAuxiliar.size());
-			if (1 != mutanteAuxiliar.get(datotmp) && 1 != mutanteAuxiliar.get(segundodato)) {
-				Integer tmp = mutanteAuxiliar.get(datotmp);
-				mutanteAuxiliar.set(datotmp, mutanteAuxiliar.get(segundodato));
-				mutanteAuxiliar.set(segundodato, tmp);
-			} else {
-				i--;
-			}
+			Integer tmp = mutanteAuxiliar.get(datotmp);
+			mutanteAuxiliar.set(datotmp, mutanteAuxiliar.get(segundodato));
+			mutanteAuxiliar.set(segundodato, tmp);
 		}
-
 		return mutanteAuxiliar;
 	}
 
 	public static Integer sumMinIndividuo(ArrayList<Integer> arrayList) {
 		// Operaciones
-		Integer minAcum = 0;
-		
-		for (Integer i = 0; i < array1.size(); i++) {
-			for (Integer j = 0; j < array2.size(); j++) {
-				if (array3.get(i) != 0) {
-					if (array1.get(i) == array2.get(j) && array2.get(i) == array1.get(j)) {
-						array3.set(j, 0);
-						if (arrayList.get(array1.get(i)) < arrayList.get(array1.get(j))) {
-							minAcum += arrayList.get(array1.get(i));
-						} else {
-							minAcum += arrayList.get(array1.get(j));
-						}
-					}
-				}
-			}
+		Integer minAcum = 0, etiqueta1 = 0, etiqueta2 = 0;
+		for (int i = 0; i < array1.size(); i++) {
+			etiqueta1 = arrayList.get(array1.get(i));
+			etiqueta2 = arrayList.get(array2.get(i));
 
+			if (etiqueta1 < etiqueta2) {
+				minAcum += etiqueta1;
+			} else {
+				minAcum += etiqueta2;
+			}
 		}
-		return minAcum;
+
+		return (minAcum);
+
 	}
 
 	public static Individuo busquedaLocal(ArrayList<Integer> arrayList, Integer minimo) {
@@ -104,17 +69,19 @@ public class Grafo {
 		ArrayList<Individuo> pob;
 		pob = (ArrayList<Individuo>) population.clone();
 
+
 		pob2.clear();
+		population2 = (ArrayList<Individuo>) population.clone();
+		population.clear();
+
+		//System.out.println(population2.get(0).minimo);
+
 		Collections.sort(pob);
 		minTmp = pob.get(0).minimo;
-		System.out.println("GANADORA");
-		for (int k=0;k<pob.get(0).list.size();k++) {
-			System.out.print(pob.get(0).list.get(k)+" ");
-		}
-		System.out.println();
-		System.out.println(minTmp + " " + cont++);
-		for (Integer i = 0; i < pob.size() - 1; i += 2) {
-			Crossover.combinar(pob.get(i).list, pob.get(i + 1).list);
+		//System.out.println(minTmp + " " + cont++);
+
+		for (Integer i = 1; i <= (pob.size()/2); i++) {
+			Crossover.combinar(pob.get(0).list, pob.get(i).list);
 			pob2.add(Crossover.hijo1);
 			pob2.add(Crossover.hijo2);
 		}
@@ -124,7 +91,6 @@ public class Grafo {
 
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		v = new int[n];
 
 		//Lectura
 		int a1 = 0, a2 = 0, a3 = 0;
@@ -146,69 +112,77 @@ public class Grafo {
 			array1.add(a1);
 			array2.add(a2);
 			array3.add(a3);
-	    }
-		
-		Integer maxArista = 0, maxAristaAcum = 0, cont = 0;
-
-		for(int i = 0; i < n;i++) {
-			while(array1.get(cont) == i) {
-				v[i]++;
-				cont++;
-				if(cont > array1.size() - 1) {
-					break;
-				}
-			}
-			
-			if (v[i] > maxAristaAcum) {
-				maxArista = i;
-				maxAristaAcum = v[i];
-			}
 		}
-		
-		System.out.println(maxArista);
-		
-			
+
+		quitarRepetidos(array1,array2,array3);
+
+
 		// Rellenar
-		ArrayList<Integer> arre = new ArrayList<Integer>();
-		for (Integer i = 2; i <= n; i++) {
-			arre.add(i);
+		//ArrayList<Integer> arre = new ArrayList<Integer>();
+		for (Integer i = 1; i <= n; i++) {
+			v.add(i);
 		}
 
 		cantPoblaciones = 100;
 
 		// Tiene que ser par
 		cantIndividuos = 50;
-	
-		
-		
-		
 
-		poblacion(maxArista, arre);
+		boolean tronar=true;
+
+		poblacion(v);
 
 		cruza();
 		minDefinitivo = minTmp;
-		boolean tronar=false;
-		int contadorAux=1;
-		while(!tronar) {
-			for (Integer j = 0; j < cantIndividuos; j++) {
-				population.add(new Individuo(busquedaLocal(pob2.get(j), sumMinIndividuo(pob2.get(j)))));
+		while(tronar) {
+
+			for (Integer i = 0; i < cantIndividuos; i++) {
+				population.add(new Individuo(busquedaLocal(pob2.get(i), sumMinIndividuo(pob2.get(i)))));
+				population.add(population2.get(i));
 			}
+
+
+			Collections.sort(population);
+
+			int k=population.size()/2;
+			int f =population.size()/2;
+			while(k!=0) {
+				population.remove(f);
+				k--;
+			}
+
+
 			if (minTmp < minDefinitivo) {
 				minDefinitivo = minTmp;
 				contador = 0;
 			} else {
 				contador++;
-				if(contador == 100) {
-					tronar=true;
+				if(contador == 150) {
+					System.out.println("El minimo final es: " + minDefinitivo);
+					System.out.print("El etiquetado final es: ");
+					for(int x = 0; x < population.get(0).list.size();x++) {
+						System.out.print(population.get(0).list.get(x) + " ");
+					}
+					tronar= !tronar;
 					break;
 				}
 			}
-			contadorAux++;
-			
+
 			cruza();
 		}
-		System.out.println("El minimo final es: " + minDefinitivo);
-		System.out.print(contadorAux);
+
+	}
+
+	public static void quitarRepetidos(ArrayList<Integer> array1, ArrayList<Integer> array2, ArrayList<Integer> array3) {
+		for (int i = 0; i < array1.size(); i++) {
+			for(int j = 0; j < array2.size();j++) {
+				if(array1.get(i) == array2.get(j) && array2.get(i) == array1.get(j)) {
+					array1.remove(j);
+					array2.remove(j);
+					array3.remove(j);
+				}
+			}
+		}
 	}
 }
 
@@ -239,3 +213,4 @@ class Individuo implements Comparable<Individuo> {
 	}
 
 }
+
